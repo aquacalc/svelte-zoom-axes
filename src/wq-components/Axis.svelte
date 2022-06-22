18a82079e3141b1a5dd54e4a159588f033d93824
aquacalc <aquacalc@yahoo.com>
2022-06-22 18:07:08 -0400

    add y-axis on right

--- src/wq-components/Axis.svelte
+++ src/wq-components/Axis.svelte
@@ -1,37 +1,42 @@
 <script>
-	// see: Tom Février's https://github.com/TomFevrier/svelte-d3-demo/blob/master/src/ScatterPlot.svelte
-	
-	import { select, selectAll } from 'd3-selection';
-	import { axisBottom, axisLeft } from 'd3-axis';
+  // see: Tom Février's https://github.com/TomFevrier/svelte-d3-demo/blob/master/src/ScatterPlot.svelte
 
-	// export let width;
-	export let height;
-	export let margin;
-	export let position;
-	export let scale;
+  import { select, selectAll } from "d3-selection";
+  import { axisBottom, axisLeft, axisRight } from "d3-axis";
 
+  export let width;
+  export let height;
+  export let margin;
+  export let position;
+  export let scale;
 
-	let transform;
-	let g;
+  let transform;
+  let g;
 
-	$: {
-		select(g).selectAll('*').remove();
+  $: {
+    select(g).selectAll("*").remove();
 
-		let axis;
+    let axis;
 
-		switch(position) {
-			case 'bottom':
-				axis = axisBottom(scale).tickSizeOuter(0).ticks(7);
-				transform = `translate(${margin.left}, ${height + margin.top})`;
-				break;
+    switch (position) {
+      case "bottom":
+        axis = axisBottom(scale).tickSizeOuter(0).ticks(7);
+        transform = `translate(${margin.left}, ${height + margin.top})`;
+        break;
 
-			case 'left':
-				axis = axisLeft(scale).tickSizeOuter(0).ticks(7);
-				transform = `translate(${margin.left}, ${margin.top})`;
-		}
+      case "left":
+        axis = axisLeft(scale).tickSizeOuter(0).ticks(7);
+        transform = `translate(${margin.left}, ${margin.top})`;
+        break;
 
-		select(g).call(axis);
-	}
+      case "right":
+        axis = axisRight(scale).tickSizeOuter(0).ticks(7);
+        transform = `translate(${width + margin.left}, ${margin.top})`;
+        break;
+    }
+
+    select(g).call(axis);
+  }
 </script>
 
-<g class='axis' bind:this={g} {transform} />
+<g class="axis" bind:this={g} {transform} />
